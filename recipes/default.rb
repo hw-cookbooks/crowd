cfb83d2ea1c9c18432683cc0b5013511ee225680
Chris Roberts <chrisroberts.code@gmail.com>
2012-10-15 11:06:23 -0700

    Create scratch dir first. Add java inclusion

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,9 @@
-include_recipe 'crowd::install'
-
 directory node[:crowd][:scratch_dir] do
   action :create
 end
+
+include_recipe 'java'
+include_recipe 'crowd::install'
 
 service 'crowd' do
   action [:enable, :start]
